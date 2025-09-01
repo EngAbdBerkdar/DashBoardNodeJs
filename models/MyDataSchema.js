276a52b39392b5b8be48deda9a87404912ebd497
EngAbdBerkdar <abdberkdarEng@outlook.com>
2025-09-01 20:46:04 +0300

    the edits project

--- models/MyDataSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const schema = mongoose.Schema;
-
-const userName = new schema({
-  userNameee: String,
-});
-const TheUserName = mongoose.model("TheUserName", userName);
-module.exports = TheUserName;
