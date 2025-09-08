fe2cc3f22bbd3d06038376a968bd053237278978
EngAbdBerkdar <abdberkdarEng@outlook.com>
2025-09-08 12:16:55 +0300

    the end

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true })); //
 app.set("view engine", "ejs");
 app.use(express.static("public")); // to use css file
